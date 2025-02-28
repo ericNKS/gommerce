908f8c1a2032a618f4bc95c4ed2395aa5bd72f9b
ericNKS <ek.silva.santos@gmail.com>
2025-02-27 23:36:43 -0300

    feat validate request

--- app/controller/user-controller.go
+++ app/controller/user-controller.go
@@ -2,8 +2,11 @@ package controller
 
 import (
 	"github.com/ericNKS/gommerce/app/models"
+	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
 )
+
+var validate = validator.New() // Inicializa o validador
 
 type UserController struct{}
 
@@ -12,8 +15,20 @@ func (uc *UserController) Store(ctx *fiber.Ctx) error {
 
 	if err := ctx.BodyParser(&user); err != nil {
 		ctx.Status(403)
-		return err
+		return ctx.JSON(fiber.Map{
+			"error": err.Error(),
+		})
 	}
 
-	return ctx.JSON(models.UserResponse{})
+	if err := validate.Struct(user); err != nil {
+		// Se houver erro de validação, retornamos um erro
+		var validationErrors []string
+		for _, err := range err.(validator.ValidationErrors) {
+			validationErrors = append(validationErrors, err.Error())
+		}
+		ctx.Status(400)
+		return ctx.JSON(fiber.Map{"error": validationErrors})
+	}
+
+	return ctx.JSON(user)
 }
--- app/models/user.go
+++ app/models/user.go
@@ -11,9 +11,9 @@ type User struct {
 }
 
 type UserCreate struct {
-	Name     string   `json:"name" validate:"required,min=4"`
-	Email    string   `json:"email" validate:"required,email"`
-	Password string   `json:"password" validate:"required,min=8"`
+	Name     string   `json:"name" validate:"required,min=4,max=100" msg:"Name is required and should have at least 4 characters"`
+	Email    string   `json:"email" validate:"required,email" msg:"A valid email is required"`
+	Password string   `json:"password" validate:"required,min=8" msg:"Password is required and should have at least 8 characters"`
 	Rule     []string `json:"rule"`
 }
 
